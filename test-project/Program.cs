c46099af9e045e07663d69288632f45bafa9288a
Levael <Levael.Savvy@gmail.com>
2023-03-22 01:01:59 +0200

    first steps

---
+++ test-project/Program.cs
@@ -0,0 +1,17 @@
+﻿//using system
+
+namespace Learning {
+    class Test {
+        static void Main (string[] args) {
+
+            Console.WriteLine("Input one:");
+            byte number_1 = Convert.ToByte(Console.ReadLine());
+
+            Console.WriteLine("Input two:");
+            byte number_2 = Convert.ToByte(Console.ReadLine());
+
+            Console.WriteLine(number_1 + number_2);
+            
+        }
+    }
+}
