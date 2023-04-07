7e1e7e9a289defccfcc8a7a7c1b899d093d88daa
Michael Levin <Levael.Savvy@gmail.com>
2023-04-07 15:28:55 +0300

    +1 test func
    params obj[] cool thing

--- test-project/Program.cs
+++ test-project/Program.cs
@@ -1,18 +1,28 @@
-﻿//using system
+using System;
 
 namespace Learning {
-    class Test {
-        static void Main (string[] args) {
+    public class Test {
+        public static void Main (string[] args) {
+            PrintParamsInfo("str", -8, 3.14, true, 'c');
+        }
+        
+        // static void Converter () {
+        //     Console.WriteLine("Input one:");
+        //     byte number_1 = Convert.ToByte(Console.ReadLine());
 
-            Console.WriteLine("Input one:");
-            byte number_1 = Convert.ToByte(Console.ReadLine());
+        //     Console.WriteLine("Input two:");
+        //     byte number_2 = Convert.ToByte(Console.ReadLine());
 
-            Console.WriteLine("Input two:");
-            byte number_2 = Convert.ToByte(Console.ReadLine());
-
-            Console.WriteLine("The sum is: " + (number_1 + number_2));
-            Console.ReadKey();
+        //     Console.WriteLine("The sum is: " + (number_1 + number_2));
+        //     Console.ReadKey();
+        // }
+        
+        static void PrintParamsInfo (params object[] parameters) {
+            string output_msg = "Type: {0}, value: {1}";
             
+            foreach (var parameter in parameters) {
+                Console.WriteLine(output_msg, parameter.GetType(), parameter);
+            }
         }
     }
 }
